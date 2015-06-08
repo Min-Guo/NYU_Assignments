4f5ead5ed53affba772ed320dcaa597e236d5942
Min Guo <xmguomin@gmail.com>
2015-06-08 14:01:54 -0400

    Add symbol structure

--- OS/Lab#1/Linker/Linker/main.c
+++ OS/Lab#1/Linker/Linker/main.c
@@ -10,18 +10,21 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+/* define symbol table */
+struct symbol
+{
+    char *symbolName;
+    char *symbolAddress;
+};
+struct symbol symbols[10];
 
 int main() {
     FILE *file;
-    file = fopen("/Users/Min/Development/NYU_Assignments/OS/Lab#1/labsamples/input-2", "r");
+    file = fopen("/Users/Min/Development/NYU_Assignments/OS/Lab#1/labsamples/input-18", "r");
     
     int n;
     int m;
     char *defcount;
-    //    char symbol[n];
-    char symbolName[16];
-    char symbol;
-    char *sAddress;
     
     //
     /* read definiton number */
@@ -31,70 +34,20 @@ int main() {
     
     /* read symbol defintion */
     for (int i = 0; i < n; i ++) {
-        fscanf(file, "%s", &symbolName);
-        fscanf(file, "%s", &sAddress);
-        m = atoi(&sAddress);
+        fscanf(file, "%s", &symbols[i].symbolName);
+        fscanf(file, "%s", &symbols[i].symbolAddress);
+        m = atoi(&symbols[i].symbolAddress);
         if (m == 0) {
             printf("NUM_EXPECTED");
+            break;
         } else
         {
-            printf("%s ", &symbolName);
-            printf("%s ", &sAddress);
+            printf("%s ", &symbols[i].symbolName);
+            printf("%s ", &symbols[i].symbolAddress);
         }
     }
     
-    //    do
-    //    {
-    //        c = fgetc(file);
-    //        if(isdigit(c)){
-    //            defcount = c - '0';
-    //            printf("%i", defcount);
-    //
-    //        }
-    //        else {
-    //            printf("Error!");
-    //        }
-    //    }
-    //    while (c != EOF);
-    
-    /*read defintion number */
-    //    do {
-    //        c = fgetc(file);
-    //        if ((c >= '0') && (c <= '9')) {
-    ////            int j = 0;
-    //            defcount = c - '0';
-    ////            j++;
-    //            printf("%i ", defcount);
-    //        }
-    //    }
-    ////        else {
-    ////            printf("Number expected!");}
-    ////        }
-    //        while (!isspace(c)); /* read one more character */
-    //
-    
-    ////
-    ///* read symbol */
-    //    for (int i = 0; i < defcount; i ++) {
-    //        do {
-    //            c = fgetc(file);
-    //            if (isalpha(c)) {
-    //                symbolName = &c;
-    //            } else {
-    //                printf("Symbol expected!");
-    //            }
-    //        } while (!isspace(c));
-    //
-    //        do {
-    //            c = fgetc(file);
-    //            if (isnumber(c)) {
-    //                int temp = c -'0';
-    //                sAddress = &temp;
-    //            }
-    //        } while (!isspace(c));
-    //    
-    //    }
-    
+  
     
     fclose(file);
     return 0;
