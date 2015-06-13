0af9d8cab0aa905445a2671ab01bad44ad596191
Min Guo <xmguomin@gmail.com>
2015-06-12 22:04:21 -0400

    Read defList by line

--- OS/Lab#1/Linker/Linker/main.c
+++ OS/Lab#1/Linker/Linker/main.c
@@ -11,6 +11,7 @@
 #include <ctype.h>
 
 FILE *file;
+char ch;
 int a = 0;
 int b = 0;
 int numModule = 0;
@@ -32,6 +33,11 @@ char tempInsAdds[4];
 int baseAddress;
 int absoluteAddress[10];
 int prevNumIns = 0;
+
+char line_buffer[512];
+char *token;
+char tokenBuffer[256][2];
+int lineNum = 0;
 
 /* define symbol table */
 struct symbolDef
@@ -58,148 +64,199 @@ struct module modules[10];
 
 int main() {
     
-    file = fopen("/Users/Min/Development/NYU_Assignments/OS/Lab#1/labsamples/input-19", "r");
+    file = fopen("/Users/Min/Development/NYU_Assignments/OS/Lab#1/labsamples/input-14", "r");
 
-    while (!feof(file)) {
-        baseAddress = 0;
-        numModule ++;
-        for(int i = 0; i < numModule; i++){
-            baseAddress += NumIns[i];
-        }
+//    while (!feof(file)) {
+//        baseAddress = 0;
+//        numModule ++;
+//        for(int i = 0; i < numModule; i++){
+//            baseAddress += NumIns[i];
+//        }
         ReadDefList(file);
-        ReadUseList(file);
-        ReadInstructions(file);
-        CheckReAdd();
-        CalculateAbAddress();
-    };
-    PrintSymbolTable();
+//        ReadUseList(file);
+//        ReadInstructions(file);
+//        CheckReAdd();
+//        CalculateAbAddress();
+//    };
+//    PrintSymbolTable();
     fclose(file);
     return 0;
 }
 
 int ReadDefList(FILE *file) {
-    /* read definiton number */
-    if ((scanValue = fscanf(file, "%s", &defcount))> 0){
-
-        idefcount = atoi(&defcount);
-        if (idefcount > 16) {
-            printf("TO_MANY_DEF_IN_MODULE");
-            exit(0);
-        } else {
-    /* read symbol defintion */
+    int tokenNum = 0;
+    fgets(line_buffer, 512, file);
+    lineNum ++;
+    printf("%s", &line_buffer);
+    token = strtok(line_buffer, " ");
+    idefcount = atoi(token);
     
-            for (int i = 0; i < idefcount; i ++) {
-                if ((scanValue = fscanf(file, "%s", &symbolDefs[i + prevDefcount].symbolName))> 0) {
-                    fscanf(file, "%s", &symbolDefs[i + prevDefcount].symbolAddress);
-                    for (int j =0; j < 4; j++) {
-                        if (!isdigit(symbolDefs[i + prevDefcount].symbolAddress[j]) && (symbolDefs[i + prevDefcount].symbolAddress[j]!= NULL)) {
-                            printf("NUM_EXPECTED");
-                            exit(0);
-                        }
-                    }
-//                    if (isalpha(&symbolDefs[i + prevDefcount].symbolAddress[0])) {
-//                        printf("NUM_EXPECTED");
-//                        exit(0);
-//                    }
-                } else {
-                    printf("SYM_EXPECTED");
+    if (idefcount > 16) {
+        printf("TO_MANY_DEF_IN_MODULE");
+        exit(0);
+    }
+    else {
+        for (int i = 0; i < idefcount; i ++) {
+            token = strtok(NULL, " ");
+            strcpy(&symbolDefs[i + prevDefcount].symbolName, token);
+            printf("symbolname is %s\n", &symbolDefs[i + prevDefcount].symbolName);
+            token = strtok(NULL, " ");
+            strcpy(&symbolDefs[i + prevDefcount].symbolAddress, token);
+            printf("symbolAdd is %s", &symbolDefs[i + prevDefcount].symbolAddress);
+            for (int j =0; j < 4; j++) {
+                if (!isdigit(symbolDefs[i + prevDefcount].symbolAddress[j]) && (symbolDefs[i + prevDefcount].symbolAddress[j]!= NULL)) {
+                    printf("Parse Error line %i: NUM_EXPECTED", lineNum);
                     exit(0);
-                    }
                 }
             }
         }
+    }
+//    token = strtok(NULL, " ");
+//    
+//    token = strtok(line_buffer, " ");
+//    printf("token is %s\n", token);
+//    token = strtok(NULL, " ");
     
+//    while( token != NULL )
+//    {
+//        strcpy(&tokenBuffer[tokenNum], token);
+//        token = strtok(NULL, " ");
+//        tokenNum ++;
+//    };
+//    idefcount = atoi(&tokenBuffer[0]);
+//    if (idefcount > 16) {
+//        printf("TO_MANY_DEF_IN_MODULE");
+//        exit(0);
+//    } else {
+//        for (int i = 0; i < idefcount; i ++) {
+//            
+//        }
+//    }
+    
+//    if (idefcount > 16) {
+//        printf("TO_MANY_DEF_IN_MODULE");
+//        exit(0);
+//    } else {
+    
+//    /* read definiton number */
+//
+//    if ((scanValue = fscanf(file, "%s", &defcount))> 0){
+//       
+//        idefcount = atoi(&defcount);
+//        if (idefcount > 16) {
+//            printf("TO_MANY_DEF_IN_MODULE");
+//            exit(0);
+//        } else {
+//    /* read symbol defintion */
+//    
+//            for (int i = 0; i < idefcount; i ++) {
+//                if ((scanValue = fscanf(file, "%s", &symbolDefs[i + prevDefcount].symbolName))> 0) {
+//                    fscanf(file, "%s", &symbolDefs[i + prevDefcount].symbolAddress);
+//                    for (int j =0; j < 4; j++) {
+//                        if (!isdigit(symbolDefs[i + prevDefcount].symbolAddress[j]) && (symbolDefs[i + prevDefcount].symbolAddress[j]!= NULL)) {
+//                            printf("NUM_EXPECTED");
+//                            exit(0);
+//                        }
+//                    }
+//                } else {
+//                    printf("SYM_EXPECTED");
+//                    exit(0);
+//                    }
+//                }
+//            }
+//        }
+//    
     return 0;
 } ;
-
-int ReadUseList(FILE *file){
-    /* read symbol declaration */
-    if ((scanValue = fscanf(file, "%s", &declareCount)) > 0){
-        ideclareCount = atoi(&declareCount);
-        if (ideclareCount > 16) {
-            printf("TO_MANY_USE_IN_MODULE");
-            exit(0);
-        } else {
-            for (int i = 0; i < ideclareCount; i++) {
-                if ((scanValue = fscanf(file, "%s", &symbolLists[i + prevDeclareCount].symbolDeclare)) > 0) {
-                    if (!isalpha(symbolLists[i + prevDeclareCount].symbolDeclare[0])) {
-                        printf("SYM_EXPECTED");
-                        exit(0);
-                    }
-                }
-            }
-        }
-    
-        prevDeclareCount += atoi(&declareCount);
-    }
-    
-    return 0;
-};
-
-int ReadInstructions(FILE *file){
-    /* read num-instructions */
-    if ((scanValue = fscanf(file, "%s", &numInstructions[numModule-1])) >0 ){
-
-        NumIns[numModule-1] = atoi(&numInstructions[numModule-1]);
-        prevNumIns += atoi(&numInstructions[numModule-1]);
-        if (prevNumIns >512) {
-            printf("TO_MANY_INSTR\n");
-            exit(0);
-        } else {
-            for (int i = 0; i < NumIns[numModule-1]; i++) {
-                if ((scanValue = fscanf(file, "%s", &tempIns))> 0) {
-                    fscanf(file, "%s ", &tempInsAdds);
-                } else {
-                    printf("ADDR_EXPECTED");
-                    exit(0);
-                }
-            }
-        }
-    }
-    return 0;
-};
-
-/* Check the relative address is valid or not*/
-int CheckReAdd(){
-    for (int i = prevDefcount ; i < atoi(&defcount); i ++) {
-        if (atoi(&symbolDefs[i].symbolAddress) > atoi(&numInstructions[numModule-1])) {
-            
-            printf("Warning: Module %i : %s to big %i (max=%i) assume zero relative.\n", numModule, &symbolDefs[i].symbolName, atoi(&symbolDefs[i].symbolAddress), (atoi(&numInstructions[numModule-1]) -1));
-            strcpy(symbolDefs[i].symbolAddress, "0");
-
-        }
-    }
-    return 0;
-};
-
-/* Absolute Address */
-
-int CalculateAbAddress(){
-    for (int i = 0; i < idefcount; i ++) {
-        absoluteAddress[i+ prevDefcount] = baseAddress + atoi(&symbolDefs[i + prevDefcount].symbolAddress);
-    }
-    prevDefcount  += atoi(&defcount);
-    return 0;
-};
-
-/* print symbol table*/
-
-int PrintSymbolTable(){
-    printf("Symbol Table\n");
-    for(int j = 0; j < prevDefcount; j++) {
-        for (int k = j + 1; k < prevDefcount; k++) {
-            if(strcmp(&symbolDefs[j].symbolName, &symbolDefs[k].symbolName)== 0){
-                strcpy(&symbolDefs[k].symbolName, &symbolDefs[k+1].symbolName);
-                strcpy(&symbolDefs[k].symbolAddress, &symbolDefs[k+1].symbolAddress);
-                absoluteAddress[k]= absoluteAddress[k+1];
-                prevDefcount -= 1;
-                printf("%s=%i Error: This variable is multiple times defined; first value used\n", &symbolDefs[j].symbolName, absoluteAddress[j]);
-            }
-        }
-        printf("%s=%i\n", &symbolDefs[j].symbolName, absoluteAddress[j]);
-    }
-
-    
-    return 0;
-};
-
+//
+//int ReadUseList(FILE *file){
+//    /* read symbol declaration */
+//    if ((scanValue = fscanf(file, "%s", &declareCount)) > 0){
+//        ideclareCount = atoi(&declareCount);
+//        if (ideclareCount > 16) {
+//            printf("TO_MANY_USE_IN_MODULE");
+//            exit(0);
+//        } else {
+//            for (int i = 0; i < ideclareCount; i++) {
+//                if ((scanValue = fscanf(file, "%s", &symbolLists[i + prevDeclareCount].symbolDeclare)) > 0) {
+//                    if (!isalpha(symbolLists[i + prevDeclareCount].symbolDeclare[0])) {
+//                        printf("SYM_EXPECTED");
+//                        exit(0);
+//                    }
+//                }
+//            }
+//        }
+//    
+//        prevDeclareCount += atoi(&declareCount);
+//    }
+//    
+//    return 0;
+//};
+//
+//int ReadInstructions(FILE *file){
+//    /* read num-instructions */
+//    if ((scanValue = fscanf(file, "%s", &numInstructions[numModule-1])) >0 ){
+//
+//        NumIns[numModule-1] = atoi(&numInstructions[numModule-1]);
+//        prevNumIns += atoi(&numInstructions[numModule-1]);
+//        if (prevNumIns >512) {
+//            printf("TO_MANY_INSTR\n");
+//            exit(0);
+//        } else {
+//            for (int i = 0; i < NumIns[numModule-1]; i++) {
+//                if ((scanValue = fscanf(file, "%s", &tempIns))> 0) {
+//                    fscanf(file, "%s ", &tempInsAdds);
+//                } else {
+//                    printf("ADDR_EXPECTED");
+//                    exit(0);
+//                }
+//            }
+//        }
+//    }
+//    return 0;
+//};
+//
+///* Check the relative address is valid or not*/
+//int CheckReAdd(){
+//    for (int i = prevDefcount ; i < atoi(&defcount); i ++) {
+//        if (atoi(&symbolDefs[i].symbolAddress) > atoi(&numInstructions[numModule-1])) {
+//            
+//            printf("Warning: Module %i : %s to big %i (max=%i) assume zero relative.\n", numModule, &symbolDefs[i].symbolName, atoi(&symbolDefs[i].symbolAddress), (atoi(&numInstructions[numModule-1]) -1));
+//            strcpy(symbolDefs[i].symbolAddress, "0");
+//
+//        }
+//    }
+//    return 0;
+//};
+//
+///* Absolute Address */
+//
+//int CalculateAbAddress(){
+//    for (int i = 0; i < idefcount; i ++) {
+//        absoluteAddress[i+ prevDefcount] = baseAddress + atoi(&symbolDefs[i + prevDefcount].symbolAddress);
+//    }
+//    prevDefcount  += atoi(&defcount);
+//    return 0;
+//};
+//
+///* print symbol table*/
+//
+//int PrintSymbolTable(){
+//    printf("Symbol Table\n");
+//    for(int j = 0; j < prevDefcount; j++) {
+//        for (int k = j + 1; k < prevDefcount; k++) {
+//            if(strcmp(&symbolDefs[j].symbolName, &symbolDefs[k].symbolName)== 0){
+//                strcpy(&symbolDefs[k].symbolName, &symbolDefs[k+1].symbolName);
+//                strcpy(&symbolDefs[k].symbolAddress, &symbolDefs[k+1].symbolAddress);
+//                absoluteAddress[k]= absoluteAddress[k+1];
+//                prevDefcount -= 1;
+//                printf("%s=%i Error: This variable is multiple times defined; first value used\n", &symbolDefs[j].symbolName, absoluteAddress[j]);
+//            }
+//        }
+//        printf("%s=%i\n", &symbolDefs[j].symbolName, absoluteAddress[j]);
+//    }
+//
+//    
+//    return 0;
+//};
+//
