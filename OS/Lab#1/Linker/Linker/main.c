583ddacce6c17e05de15c3a134557b6d25977287
Min Guo <xmguomin@gmail.com>
2015-06-14 12:05:37 -0400

    Fix the bug of symbolList

--- OS/Lab#1/Linker/Linker/main.c
+++ OS/Lab#1/Linker/Linker/main.c
@@ -34,8 +34,12 @@ int prevTotalDefcount = 0;
 int prevTotalDeclareCount = 0;
 int totalLengthModule = 0;
 int i = 0;
-int j = 0;
+//int j = 0;
 int k = 0;
+int k_parseTwo = 0;
+int baseAdd_parseTwo = 0;
+int i_parseTwo = 0;
+int j_parseTwo = 0;
 /* define symbol table */
 struct symbolDef
 {
@@ -58,17 +62,28 @@ struct programText
 };
 struct programText programTexts[256];
 
+struct programText_parseTwo
+{
+    char addType;
+    int instruction;
+};
+struct programText_parseTwo programTexts_parseTwo[256];
+
 
 
 
 int main() {
 
     
-    file = fopen("/Users/Min/Development/NYU_Assignments/OS/Lab#1/labsamples/input-1", "r");
-//    ParseOne(file);
-    ParseTwo(file);
-
+    file = fopen("/Users/Min/Development/NYU_Assignments/OS/Lab#1/labsamples/input-19", "r");
+    ParseOne(file);
     fclose(file);
+    
+    
+//    file = fopen("/Users/Min/Development/NYU_Assignments/OS/Lab#1/labsamples/input-1", "r");
+//    ParseTwo(file);
+//
+//    fclose(file);
     return 0;
 }
 
@@ -171,7 +186,6 @@ int ParseOne(FILE* file){
                             }
                         }
                     } else if (listType == 1){
-                        
                         if (operationNum == -1) {
                             operationNum = atoi(token);
                             declareCount = operationNum;
@@ -183,14 +197,17 @@ int ParseOne(FILE* file){
                                 operationNum = -1;
                             }
                         } else {
-                            strcpy(&symbolLists[prevTotalDeclareCount + j].symbolDeclare, token);
-                            if (!isalpha(symbolLists[prevTotalDeclareCount + j].symbolDeclare[0])) {
+                            
+                            strcpy(&symbolLists[prevTotalDeclareCount].symbolDeclare, token);
+                           
+                            if (!isalpha(symbolLists[prevTotalDeclareCount].symbolDeclare[0])) {
                                 printf("Parse Error line %i: SYM_EXPECTED", lineNum);
                                 exit(0);
                             } else{
                                 //                                    printf("SymbolList[%i] is %s ", j, &symbolLists[prevTotalDeclareCount + j].symbolDeclare);
                                 operationNum--;
-                                j++;
+                                 prevTotalDeclareCount++;
+//                                j++;
                                 if (operationNum == 0) {
                                     listType = 2;
                                     operationNum = -1;
@@ -230,7 +247,7 @@ int ParseOne(FILE* file){
                                     middleState = false;
                                     moduleNumber++;
                                     prevTotalDefcount += defCount;
-                                    prevTotalDeclareCount += declareCount;
+//                                    prevTotalDeclareCount += declareCount;
                                     
                                 }
                             } else {
@@ -295,19 +312,19 @@ int ParseTwo(FILE* file){
                             
                         } else {
                             if (middleState) {
-                                strcpy(&symbolDefs[i].symbolAddress, token);
-                                for (int t =0; t < 4; t++) {
-                                    
-                                    if (!isdigit(symbolDefs[i].symbolAddress[t]) && (symbolDefs[i].symbolAddress[t]!= NULL)) {
-//                                        printf("Parse Error line %i: NUM_EXPECTED", lineNum);
-                                        exit(0);
-                                    }
-                                }
+//                                strcpy(&symbolDefs[i].symbolAddress, token);
+//                                for (int t =0; t < 4; t++) {
+//                                    
+//                                    if (!isdigit(symbolDefs[i].symbolAddress[t]) && (symbolDefs[i].symbolAddress[t]!= NULL)) {
+////                                        printf("Parse Error line %i: NUM_EXPECTED", lineNum);
+//                                        exit(0);
+//                                    }
+//                                }
                                 
                                 //                                    printf("Symbol[%i]Address is % s\n", i,  &symbolDefs[i].symbolAddress);
                                 defOperationNum-- ;
                                 middleState = false;
-                                i++;
+                                i_parseTwo++;
                                 if (defOperationNum == 0) {
                                     listType = 1;
                                     defOperationNum = -1;
@@ -315,7 +332,7 @@ int ParseTwo(FILE* file){
                                     //                                        CalculateAbAddress();
                                 }
                             } else {
-                                strcpy(&symbolDefs[i].symbolName, token);
+//                                strcpy(&symbolDefs[i].symbolName, token);
 //                                                                    printf("Symbol[%i]Name is %s ", i,  &symbolDefs[i].symbolName);
                                 middleState = true;
                             }
@@ -333,19 +350,19 @@ int ParseTwo(FILE* file){
                                 operationNum = -1;
                             }
                         } else {
-                            strcpy(&symbolLists[prevTotalDeclareCount + j].symbolDeclare, token);
-                            if (!isalpha(symbolLists[prevTotalDeclareCount + j].symbolDeclare[0])) {
-//                                printf("Parse Error line %i: SYM_EXPECTED", lineNum);
-                                exit(0);
-                            } else{
+//                            strcpy(&symbolLists[prevTotalDeclareCount + j].symbolDeclare, token);
+//                            if (!isalpha(symbolLists[prevTotalDeclareCount + j].symbolDeclare[0])) {
+////                                printf("Parse Error line %i: SYM_EXPECTED", lineNum);
+//                                exit(0);
+//                            } else{
                                 //                                    printf("SymbolList[%i] is %s ", j, &symbolLists[prevTotalDeclareCount + j].symbolDeclare);
                                 operationNum--;
-                                j++;
+                                j_parseTwo++;
                                 if (operationNum == 0) {
                                     listType = 2;
                                     operationNum = -1;
                                 }
-                            }
+//                            }
                         }
                         
                         
@@ -353,10 +370,10 @@ int ParseTwo(FILE* file){
                         if (ProOperationNum == -1) {
                             ProOperationNum = atoi(token);
                             lengthModule = ProOperationNum;
-                            totalLengthModule += lengthModule;
-                            CheckReAdd();
-                            CalculateAbAddress();
-                            baseAddress += ProOperationNum;
+//                            totalLengthModule += lengthModule;
+//                            CheckReAdd();
+//                            CalculateAbAddress();
+                            
                             if (totalLengthModule > 512) {
 //                                printf("Parse Error line %i: TO_MANY_INSTR", lineNum);
                                 exit(0);
@@ -368,35 +385,42 @@ int ParseTwo(FILE* file){
                             }
                         } else {
                             if (middleState) {
-                                strcpy(&programTexts[k].instruction, token);
+//                                strcpy(&programTexts[k].instruction, token);
                                 tempIns = atoi(token);
+                                programTexts_parseTwo[k_parseTwo].instruction = tempIns;
                                 int firstDigit;
                                 int lastThreeDigit;
                                 firstDigit = tempIns/1000;
                                 lastThreeDigit = tempIns % 1000;
-                                if (strcmp(&programTexts[k].addType, "I") == 0) {
-                                    printf("I %i is %s\n",k, &programTexts[k].instruction);
-                                } else if (strcmp(&programTexts[k].addType, "A") == 0){
+                                if (strcmp(&programTexts_parseTwo[k_parseTwo].addType, "I") == 0) {
+                                    printf("I  is %i\n", programTexts_parseTwo[k_parseTwo].instruction);
+                                } else if (strcmp(&programTexts_parseTwo[k_parseTwo].addType, "A") == 0){
                                     if (lastThreeDigit > 512) {
-                                        programTexts[k].instruction = firstDigit * 1000 + 0;
-                                        printf("%i  Error: Absolute address exceeds machine size; zero used\n", programTexts[k].instruction);
+                                        programTexts_parseTwo[k_parseTwo].instruction = firstDigit * 1000 + 0;
+                                        printf("%i  Error: Absolute address exceeds machine size; zero used\n", programTexts_parseTwo[k_parseTwo].instruction);
                                     } else {
-                                        printf("%s\n", &programTexts[k].instruction);
+                                        
+//                                      printf("%s\n", &programTexts[k].instruction);
                                     }
-                                } else if (strcmp(&programTexts[k].addType, "E") == 0) {
+                                } else if (strcmp(&programTexts_parseTwo[k_parseTwo].addType, "E") == 0) {
                                     if (lastThreeDigit > (declareCount - 1)) {
                                         printf("Error: External address exceeds length of uselist; treated as immediate\n");
                                     } else {
-                                        printf("%s\n", &programTexts[k].instruction);
+                                        for (int m = 0; m < i_parseTwo + 1; m++) {
+                                            if (strcmp(&symbolLists[j_parseTwo - declareCount + lastThreeDigit], &symbolDefs[m].symbolName) == 0) {
+                                                programTexts_parseTwo[k_parseTwo].instruction = firstDigit * 1000 + symbolDefs[m].symbolAbsoluteAddress;
+                                                printf("DefName %s External %i\n", &symbolDefs[m].symbolName, programTexts_parseTwo[k_parseTwo].instruction);
+                                            }
+                                        }
                                     }
-                                } else if (strcmp(&programTexts[k].addType, "R") == 0) {
-                                    programTexts[k].instruction += baseAddress;
-                                    printf("baseaddress is %i  ,R is %i\n", baseAddress, programTexts[k].instruction);
+                                } else if (strcmp(&programTexts_parseTwo[k_parseTwo].addType, "R") == 0) {
+                                    programTexts_parseTwo[k_parseTwo].instruction += baseAdd_parseTwo;
+                                    printf("baseaddress is %i  ,R is %i\n", baseAdd_parseTwo, programTexts_parseTwo[k_parseTwo].instruction);
                                 }
                                 
 //                                                                    printf("AddType is %s   Instrution is %s\n", &programTexts[k].addType,  &programTexts[k].instruction);
                                 ProOperationNum--;
-                                k++;
+                                k_parseTwo++;
                                 middleState = false;
                                 if (ProOperationNum == 0) {
                                     listType = 0;
@@ -405,10 +429,11 @@ int ParseTwo(FILE* file){
                                     moduleNumber++;
                                     prevTotalDefcount += defCount;
                                     prevTotalDeclareCount += declareCount;
+                                    baseAdd_parseTwo += lengthModule;
                                     
                                 }
                             } else {
-                                strcpy(&programTexts[k].addType, token);
+                                strcpy(&programTexts_parseTwo[k_parseTwo].addType, token);
                                 //                                    printf("\n");
                                 //                                    printf("AddType[%i] is %s ", k, &programTexts[k].addType);
                                 middleState = true;
@@ -425,14 +450,14 @@ int ParseTwo(FILE* file){
         }
         
     }
-    if (feof(file) && (defOperationNum != -1)) {
-//        printf("Parse Error line %i: SYM_EXPECTED", lineNum);
-        exit(0);
-    }
-    if (feof(file) && (ProOperationNum != -1)) {
+//    if (feof(file) && (defOperationNum != -1)) {
+////        printf("Parse Error line %i: SYM_EXPECTED", lineNum);
+//        exit(0);
+//    }
+//    if (feof(file) && (ProOperationNum != -1)) {
 //        printf("Parse Error line %i: ADDR_EXPECTED", lineNum);
-        exit(0);
-    }
+//        exit(0);
+//    }
 //    PrintSymbolTable();
     return 0;
 }
