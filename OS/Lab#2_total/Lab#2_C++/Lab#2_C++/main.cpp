f7e371a0439e2ead563c7ad73a7ef28bcded2a60
Min Guo <xmguomin@gmail.com>
2015-06-27 16:19:20 -0400

    Input0-3 R2 works

--- OS/Lab#2_total/Lab#2_C++/Lab#2_C++/main.cpp
+++ OS/Lab#2_total/Lab#2_C++/Lab#2_C++/main.cpp
@@ -109,7 +109,7 @@ int main(int argc, const char * argv[]) {
     readRandNum(file);
     fclose(file);
     Scheduler scheduler;
-    file = fopen("/Users/Min/Development/NYU_Assignments/OS/Lab#2_total/lab2_assign/input6", "r");
+    file = fopen("/Users/Min/Development/NYU_Assignments/OS/Lab#2_total/lab2_assign/input5", "r");
     parse(file, &scheduler);
     fclose(file);
     Process runningProcess = {0, 0, false, 0, false, 0, false, 0, false};
